650fbbd8e534921471295230b6ba3d43ea7748a6
Nicolas Venzmer <nicolas.venzmer@rga.com>
2024-11-03 20:18:48 -0300

    refactor: change naming of files

--- src/components/ProgressIndicator/ProgressIndicator.js
+++ src/components/ProgressIndicator/ProgressIndicator.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './ProgressIndicator.css';
 
-function ProgressIndicator({ currentStep, totalSteps }) {
-  return (
-    <div className="progress-indicator">
-      <p>QUESTION {currentStep} OF {totalSteps}</p>
-      <div className="dots">
-        {[...Array(totalSteps)].map((_, index) => (
-          <span key={index} className={`dot ${index < currentStep ? 'active' : ''}`} />
-        ))}
-      </div>
-    </div>
-  );
+function ProgressIndicator({currentStep, totalSteps, label}) {
+    return (
+        <div className="progress-container">
+            <div className="progress">
+                <p>{label} {currentStep} OF {totalSteps}:</p>
+                <div className="progress-dots">
+                    {Array.from({length: totalSteps}).map((_, index) => (
+                        <span
+                            key={index}
+                            className={index + 1 === currentStep ? 'dot active' : 'dot'}
+                        ></span>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default ProgressIndicator;
